9df285f7fdb7ed0866d0c3b92356fe6f04c9b1e1
Aktar <apaktar@gmail.com>
2024-11-05 13:30:30 +0600

    Message edited

--- src/HelloIntgelliJ.java
+++ src/HelloIntgelliJ.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class HelloIntgelliJ {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    System.out.println("Aktar from NSW-BD, Intellij!");
+        System.out.println("Aktar from NSW-BD, Intellij!");
         String message = scanner.nextLine();
+        System.out.println(message);
+
 }
 }
